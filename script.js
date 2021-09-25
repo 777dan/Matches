a378786ef3a8b02ee0547314defece8c42a67deb
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-09-25 11:54:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let n = 13;
+let str = " ";
+for (let i = 0; i < n; i++) {
+    str += "|";
+}
+alert(n + str);
+while (n > 0) {
+    alert("You turn!");
+    let m = prompt("Get the matches");
+    if (isNaN(m) || m < 0 || m > 3) {
+        break;
+    }
+    n -= m;
+    let str = " ";
+    for (let i = 0; i < n; i++) {
+        str += "| ";
+    }
+    alert(n + str);
+    if (n <= 0) {
+        alert("You win!");
+        break;
+    }
+    alert("My turn");
+    let rand = Math.floor(Math.random() * 3) + 1;
+    n -= rand;
+    for (let i = 0; i < n; i++) {
+        str += "| ";
+    }
+    alert(n + str);
+}
