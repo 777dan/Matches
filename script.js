b76af8054eac73ee48345344147d746120962a61
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-09-25 12:01:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
-let n = 13;
+// let n = 13;
+// let str = " ";
+// for (let i = 0; i < n; i++) {
+//     str += "|";
+// }
+// alert(n + str);
+// while (n > 0) {
+//     alert("You turn!");
+//     let m = prompt("Get the matches");
+//     if (isNaN(m) || m < 0 || m > 3) {
+//         break;
+//     }
+//     n -= m;
+//     let str = " ";
+//     for (let i = 0; i < n; i++) {
+//         str += "| ";
+//     }
+//     alert(n + str);
+//     if (n <= 0) {
+//         alert("You win!");
+//         break;
+//     }
+//     alert("My turn");
+//     let rand = Math.floor(Math.random() * 3) + 1;
+//     n -= rand;
+//     for (let i = 0; i < n; i++) {
+//         str += "| ";
+//     }
+//     alert(n + str);
+// }
+
+let n = prompt("Введите общее количество спичек");
 let str = " ";
 for (let i = 0; i < n; i++) {
     str += "|";
@@ -15,7 +46,13 @@ while (n > 0) {
     for (let i = 0; i < n; i++) {
         str += "| ";
     }
-    alert(n + str);
+    if (n % 4 == 0) {
+        alert("You win!");
+        break;
+    }
+    else {
+        alert(n + str);
+    }
     if (n <= 0) {
         alert("You win!");
         break;
@@ -26,5 +63,11 @@ while (n > 0) {
     for (let i = 0; i < n; i++) {
         str += "| ";
     }
-    alert(n + str);
+    if (n % 4 == 0) {
+        alert("I win!");
+        break;
+    }
+    else {
+        alert(n + str);
+    }
 }
